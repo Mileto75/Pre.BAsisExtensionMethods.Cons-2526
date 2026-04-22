ddbd0198f067ea06bba23bd004e3b78a1f620048
Mileto Di Marco <mileto.di.marco@howest.be>
2026-04-22 09:28:44 +0200

    Add minimum code

--- Pre.BAsisExtensionMethods.Cons/Program.cs
+++ Pre.BAsisExtensionMethods.Cons/Program.cs
@@ -13,18 +13,36 @@ namespace Pre.BAsisExtensionMethods.Cons
             var wordsArray = words.Split(" ");
             var numberOfWords = wordsArray.Count();
             Console.WriteLine($"Number of words = {numberOfWords}");
-            //string character counter
-            
+            //string letter counter
+            int numberOfLetters = 0;
+            foreach(var character in words)
+            {
+                if(character != ' ')
+                    numberOfLetters++;
+            }
+            Console.WriteLine($"Number of letters = {numberOfLetters}");
+
             //int isEven checker
-            
+            //number on position x
+            if (numbers.ToArray()[1] %2 == 0)
+                Console.WriteLine("Even");
+            else
+                Console.WriteLine("OnEven");
+
             //Ienumerable int kleinste getal
-            
+            var minimum = numbers.ToArray()[0];
+            foreach(var number in numbers)
+            {
+                if (minimum > number)
+                    minimum = number;
+            }
+            Console.WriteLine($"Smallest number = {minimum}");
             //ienumerable string random waarde uit lijst retourneren
-            
+
             //return all even numbers
-            
+
             //mapping
-            
+
             //Extra:generic ienumerable random waarde retourneren uit lijst
         }
     }
