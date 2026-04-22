a53ef7a20eeeca41b473c68dff133e46b7931087
Mileto Di Marco <mileto.di.marco@howest.be>
2026-04-22 10:15:31 +0200

    Add string extension methods

---
+++ Pre.BAsisExtensionMethods.Cons/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre.BasisExtensionMethods.Cons.Extensions
+{
+    //static class for extension methods
+    public static class StringExtensions
+    {
+        //define static methods with this parameter keyword
+        public static int CountWords(this string words)
+        {
+            if (string.IsNullOrEmpty(words))
+                return 0;
+            var wordsArray = words.Split(" ");
+            return wordsArray.Count();
+        }
+        public static int CountLetters(this string words)
+        {
+            if (string.IsNullOrEmpty(words))
+                return 0;
+            int numberOfLetters = 0;
+            foreach (var character in words)
+            {
+                if (character != ' ')
+                    numberOfLetters++;
+            }
+            return numberOfLetters;
+        }
+    }
+}
--- Pre.BAsisExtensionMethods.Cons/Program.cs
+++ Pre.BAsisExtensionMethods.Cons/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Channels;
+﻿using Pre.BasisExtensionMethods.Cons;
+using Pre.BasisExtensionMethods.Cons.Extensions;
+using Pre.BasisExtensionMethods.Core.Entities;
+using System.Threading.Channels;
 
 namespace Pre.BAsisExtensionMethods.Cons
 {
@@ -10,17 +13,15 @@ namespace Pre.BAsisExtensionMethods.Cons
             string words = "One Two Three Four Five Six Seven Eight Nine Ten";
             IEnumerable<int> numbers = [1, 2, -3, 4, 5, 6, 7, 8, 9, 10];
             //string word counter
-            var wordsArray = words.Split(" ");
-            var numberOfWords = wordsArray.Count();
-            Console.WriteLine($"Number of words = {numberOfWords}");
+            Console.WriteLine($"Number of words = {words.CountWords()}");
             //string letter counter
-            int numberOfLetters = 0;
-            foreach(var character in words)
-            {
-                if(character != ' ')
-                    numberOfLetters++;
-            }
-            Console.WriteLine($"Number of letters = {numberOfLetters}");
+            //int numberOfLetters = 0;
+            //foreach(var character in words)
+            //{
+            //    if(character != ' ')
+            //        numberOfLetters++;
+            //}
+            Console.WriteLine($"Number of letters = {words.CountLetters()}");
 
             //int isEven checker
             //number on position x
@@ -38,11 +39,37 @@ namespace Pre.BAsisExtensionMethods.Cons
             }
             Console.WriteLine($"Smallest number = {minimum}");
             //ienumerable string random waarde uit lijst retourneren
-
+            var random = new Random();
+            var randomIndex = random.Next(0, wordsArray.Count() - 1);
+            var randomWord = wordsArray.ToArray()[randomIndex];
+            Console.WriteLine($"Random word = {randomWord}");
             //return all even numbers
-
-            //mapping
-
+            List<int> evenNumbers = new();
+            foreach(var number in numbers)
+            {
+                if (number % 2 == 0)
+                    evenNumbers.Add(number);
+            }
+            Console.WriteLine($"Even numbers = {string.Join(" ",evenNumbers)}");
+            //mapping from entity to movie
+            var movie = new Movie
+            {
+                Title = "Deadpool vs Wolverine",
+                GenreId = 1,
+                Genre = "Action Marvel",
+                Actors = new List<string> { "Ryan Renolds","Hugh Jackman" },
+                YearPublished = new DateTime(2022,12,25),
+                Id = 1
+            };
+            var moviesIndexViewModel = new MoviesIndexViewModel
+            {
+                Title = movie.Title,
+                Genre = movie.Genre,
+                Actors = movie.Actors
+            };
+            Console.WriteLine($"MoviesIndexVieModel:{moviesIndexViewModel.Title}, " +
+                $"{moviesIndexViewModel.Genre}, {moviesIndexViewModel.Actors}");
+            
             //Extra:generic ienumerable random waarde retourneren uit lijst
         }
     }
